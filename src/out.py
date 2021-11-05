b07f5e620fd27f65fb215c059fd538c865cdc16d
Joao <joao@email.com>
2021-11-05 16:17:16 -0300

    Add Setup

---
+++ src/out.py
@@ -0,0 +1,13 @@
+def main():
+	a=3.0
+	b=4.0
+	
+	c
+	a=a**
+	b=b**
+	c=a+b
+	c=c**
+	print(c)
+	
+if __name__ == "__main__":
+	main()
--- src/setap.py
+++ src/setap.py
@@ -1,10 +1,33 @@
 from parser.parser import tokenize
+from parser.structer import check
+import sys
 
 
 def main():
-    text_file = ''
+    name = sys.argv[1]
+    print(name)
+    text_file = []
     with open(name) as fp:
-        text_file = fp
+        for i in fp:
+            text_file.append(i)
+    text_file = ''.join(text_file)
+    print(type(text_file))
+    valid = check(text_file)
+    print(valid)
+    if valid == 0:
+        text_out = tokenize(text_file)
+    else:
+        print(valid)
+        return
+    text_out += 'if __name__ == "__main__":'
+    text_out += chr(10)
+    text_out += chr(9)
+    text_out += 'main()'
+    text_out += chr(10)
+    with open('out.py', 'w') as fp:
+        fp.write(text_out)
+    print("Transplation OK!")
+
 
 
 if __name__ == "__main__":
